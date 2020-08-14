7ae041dd64cc212995ffe628b4fb066410a3a964
oscarwiberg <oscar.wiberg@live.se>
2020-08-14 13:29:55 +0200

    working with the modal window

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const NavbarComponent = () => {
     <div>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="/" id="nav-logo">
-          OSCAR WIBERG.
+          OSCAR WIBERG
         </Navbar.Brand>
         <Nav id="nav-links" className="mr-auto">
           <Nav.Link id="nav-link" as={Link} to="/">
@@ -21,19 +21,6 @@ const NavbarComponent = () => {
             PROJECTS
           </Nav.Link>
         </Nav>
-        {/* <Nav id="nav-links">
-          <Link id="nav-link" to="/">
-            Home
-          </Link>
-
-          <Link id="nav-link" to="/about">
-            About
-          </Link>
-
-          <Link id="nav-link" to="/projects">
-            Projects
-          </Link>
-        </Nav> */}
       </Navbar>
     </div>
   );
--- src/components/pages/Jumbo.jsx
+++ src/components/pages/Jumbo.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Jumbo.css';
+import { Modal, Button } from 'react-bootstrap';
 
 const Jumbo = () => {
+  const Example = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+      <>
+        <Button id="cta" variant="primary" onClick={handleShow}>
+          MY RESUME
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Resume</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img className="img-cv" src="/images/cv.jpg" alt="cv"></img>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  };
+
   return (
     <div>
       <div className="text-container">
@@ -10,6 +40,8 @@ const Jumbo = () => {
           I'M A JUNIOR DEVELOPER FROM STOCKHOLM. TAKE A LOOK AT MY RESUME AND MY
           EARLIER PROJECTS.
         </p>
+        <Example />
+        {/* <button>MY RESUME</button> */}
       </div>
       <img className="img-ny" src="/images/ny.jpg" alt="new-york"></img>
     </div>
